31b4de32e40b3be47f6b58867a918d387ab3759e
Leandro Andrade de Oliveira <leandro.aoliveira@localiza.com>
2022-10-14 18:03:44 -0300

    After

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -6,9 +6,10 @@ export interface HeadingProps {
     size?: 'sm' | 'md' | 'lg';
     children: ReactNode;
     asChild?: boolean;
+    className?: string;
 }
 
-export function Heading({ size = 'md', children, asChild }: HeadingProps) {
+export function Heading({ size = 'md', children, asChild, className }: HeadingProps) {
     const Component = asChild ? Slot : 'h2';
 
     return (
@@ -20,6 +21,7 @@ export function Heading({ size = 'md', children, asChild }: HeadingProps) {
                     'text-xl': size === 'md',
                     'text-2xl': size === 'lg',
                 },
+                className,
             ) }
         >
             { children }
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -6,9 +6,10 @@ export interface TextProps {
     size?: 'sm' | 'md' | 'lg';
     children: ReactNode;
     asChild?: boolean;
+    className?: string;
 }
 
-export function Text({ size = 'md', children, asChild }: TextProps) {
+export function Text({ size = 'md', children, asChild, className }: TextProps) {
     const Component = asChild ? Slot : 'span';
 
     return (
@@ -20,6 +21,7 @@ export function Text({ size = 'md', children, asChild }: TextProps) {
                     'text-sm': size === 'md',
                     'text-md': size === 'lg',
                 },
+                className,
             ) }
         >
             { children }
